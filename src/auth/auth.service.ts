99f0c83ea21255013f406dd137090413596d83e2
Visinho <Elvisvisinho@rocketmail.com>
2024-04-03 19:29:11 +0100

    Signup logic WIP

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDto } from 'src/dtos';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthService {
 
     constructor(private prismaService: PrismaService) {}
 
-    signup(dto: AuthDto) {
+    hashData(data: string) {
+        return bcrypt.hash(data, 10);
+    }
+
+    async signup(dto: AuthDto) {
+        const hash = await this.hashData(dto.password);
+        const newUser = this.prismaService.user.create({
+            data: {
+                email: dto.email,
+                hash
+            }
+        })
     }
 
     signin() {
